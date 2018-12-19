ada2e18063c2a29f9cb576b5bef9c7c6e6a6889a
Bayu Abi Pamungkas <bayuabi31@gmail.com>
2018-12-19 17:58:00 +0800

    revisi kedua

--- ESP8266_AutoNAS.ino
+++ ESP8266_AutoNAS.ino
@@ -1,13 +1,10 @@
-#include <FirebaseArduino.h>
-
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-const char* SSID = "Lab_Elka";
-const char* PASS = "daftaraslabdulu";
-
-#define FIREBASE_HOST "jamur-ca24c.firebaseio.com" 
-#define FIREBASE_AUTH "lDoJVczo5IoOg0fc0KsEpS4M7i8HMvstzl0IB9WQ"
+const char* WIFI_SSID = "YOUR WIFI SSID";
+const char* WIFI_PASS = "YOUS WIFI PASSWORD";
+const char* NIM = "YOUR NIM";
+const char* PASS = "YOUR SIAM PASSWORD"
 
 void setup() {
   Serial.begin(9600);
@@ -19,20 +16,17 @@ void setup() {
   HTTPClient http;
   http.begin("http://nas.ub.ac.id/ac_portal/login.php");
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  int httpCode = http.POST("opr=pwdLogin&userName=165060300111007&pwd=bayumalang1998&rememberPwd=1");
+  String kirim = "opr=pwdLogin&userName=";
+  kirim += NIM;
+  kirim += "&pwd=";
+  kirim += PASS;
+  kirim += "&rememberPwd=1";
+  int httpCode = http.POST(kirim);
   if(httpCode > 0){
-    Serial.println(http.getString());
-    
+    Serial.println(http.getString());   
   }
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
 void loop() {
-  //Serial.println(Firebase.getString("wereng-app/FREKUENSI"));
-  Firebase.setInt("suhu", 7);
-  if(Firebase.failed()){
-    Serial.println("error");
-    Serial.println(Firebase.error());
-  }
-  delay(500);
+
 }
